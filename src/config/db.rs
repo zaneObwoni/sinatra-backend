35b325cd023f96d0f619eee0ef5e043836eeb5d1
bonfaceZane <zaneobwoni@gmail.com>
2022-09-28 17:30:18 +0200

    added mongo db

---
+++ src/config/db.rs
@@ -0,0 +1,22 @@
+use mongodb::{options::ClientOptions, Client};
+use mongodb::bson::doc;
+
+
+pub async fn get_mongo_client() -> Result<Client, mongodb::error::Error> {
+    let mongo_uri = std::env::var("MONGO_URI")
+        .expect("😱 MONGO_URI environment must be set");
+
+    let mut client_options = ClientOptions::parse(&mongo_uri).await.unwrap();
+
+    client_options.app_name = Some("sinatra".to_string());
+    let client = Client::with_options(client_options)?;
+
+    // Ping the server to see if you can connect to the cluster
+    client
+        .database("admin")
+        .run_command(doc! {"ping": 1}, None)
+        .await?;
+    println!("🤯 CONNECTED TO DB SUCCESSFULLY!!!");
+
+    Ok(client)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-use actix_web::{HttpServer, App, middleware::Logger, web};
+mod config;
+
+use actix_web::{HttpServer, App, middleware::Logger, web, Responder};
+use config::db::get_mongo_client;
+use dotenv;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-   std::env::set_var("RUST_LOG", "debug");
+// intialize_env vars
+    dotenv::dotenv().ok();
+
+    // setting logger
+   std::env::set_var("RUST_LOG", "actix_web=debug");
    std::env::set_var("RUST_BACKTRACE", "1");
    env_logger::init();
+
+    let client = get_mongo_client().await;
 
     HttpServer::new(move || {
         let logger = Logger::default();
         App::new()
+            // link app to mongo db
+            .app_data(client.clone())
             .wrap(logger)
             .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .route("/", web::get().to(hello))
     })
     .bind(("127.0.0.1", 80))?
     .run()
     .await
+}
+
+
+async fn hello() -> impl Responder {
+    format!("Hello fellow Rustacean!")
 }
